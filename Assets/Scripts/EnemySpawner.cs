71829a553a0084faa2716c08df5325f98e7c7a0b
Ömer Can Baltacı <152120131103@ogrenci.ogu.edu.tr>
2021-02-12 18:36:53 +0300

    Basics

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public int numberOfEnemies;
+    private int xPos;
+    private int zPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (numberOfEnemies > 0)
+        {
+            xPos = Random.Range(-10, 13);
+            zPos = Random.Range(0, 12);
+
+            GameObject instantiatedEnemy = Instantiate(enemy, new Vector3(xPos, 1f, zPos), Quaternion.identity);
+            instantiatedEnemy.gameObject.tag = "Enemy";
+
+            yield return new WaitForSeconds(0.001f);
+
+            numberOfEnemies--;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    private Vector3 input;
+    private Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        velocity = input * speed;
+        rb.velocity = velocity;
+    }
+}

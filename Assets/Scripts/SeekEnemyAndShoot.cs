b3d8fe7d90a5fd552c348bbe5b3ddc7328afc416
Ömer Can Baltacı <152120131103@ogrenci.ogu.edu.tr>
2021-02-14 13:51:44 +0300

    Name update

--- Assets/Scripts/SeekEnemyAndShoot.cs
+++ Assets/Scripts/SeekEnemyAndShoot.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SeekEnemy : MonoBehaviour
+public class SeekEnemyAndShoot : MonoBehaviour
 {
     private Transform target;
     public string targetTag;
